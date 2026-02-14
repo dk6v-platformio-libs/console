4dcb59eb445dcc8c0a82cbbba997eb77724b94d7
Korobkov Dmitry <dmitry.korobkov.nn@gmail.com>
2026-02-14 21:45:34 +0300

    feat: console logger library

---
+++ src/console/Console.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2026 Dmitry Korobkov <dmitry.korobkov.nn@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <cstdio>
+#include <cstdarg>
+#include <cstring>
+
+#include "Console.h"
+
+#ifndef SERIAL_BAUD_RATE
+#define SERIAL_BAUD_RATE 115200
+#endif
+
+namespace console
+{
+    Console::Console() : serial(&Serial),
+                         baudrate(SERIAL_BAUD_RATE),
+                         mBuffer{0}
+    {
+        if (serial)
+        {
+            serial->begin(this->baudrate);
+        }
+    }
+
+    Console::~Console() {}
+
+    Console &Console::setSerial(HardwareSerial &serial)
+    {
+        this->serial = &serial;
+        if (this->serial)
+        {
+            this->serial->begin(this->baudrate);
+        }
+        return *this;
+    }
+
+    Console &Console::setBaudRate(unsigned long baudrate)
+    {
+        this->baudrate = baudrate;
+        if (serial)
+        {
+            serial->begin(this->baudrate);
+        }
+        return *this;
+    }
+
+    void Console::log(const char *fmt, ...)
+    {
+        if (!serial)
+            return;
+
+        va_list ptr;
+        va_start(ptr, fmt);
+        vsnprintf(mBuffer, sizeof(mBuffer) - 1, fmt, ptr);
+        va_end(ptr);
+
+        this->flush();
+    }
+
+    void Console::format(const char *fmt, ...)
+    {
+        if (!serial)
+            return;
+
+        size_t length = strlen(mBuffer);
+        char *npos = mBuffer + length;
+        size_t remaining = sizeof(mBuffer) - length - 1;
+
+        va_list args;
+        va_start(args, fmt);
+        vsnprintf(npos, remaining, fmt, args);
+        va_end(args);
+    }
+
+    void Console::flush()
+    {
+        if (!serial)
+            return;
+
+        if (strlen(mBuffer) > 0)
+        {
+            serial->println(mBuffer);
+            mBuffer[0] = '\0';
+        }
+    }
+
+    void Console::vformat(const char *fmt, va_list args)
+    {
+        if (!serial)
+            return;
+
+        size_t length = strlen(mBuffer);
+        char *npos = mBuffer + length;
+        size_t remaining = sizeof(mBuffer) - length - 1;
+
+        vsnprintf(npos, remaining, fmt, args);
+    }
+
+    void Console::vlog(const char *fmt, va_list args)
+    {
+        this->vformat(fmt, args);
+        this->flush();
+    }
+
+    // Console wrapper functions
+
+    void log(const char *fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        Console::getInstance().vlog(fmt, args);
+        va_end(args);
+    }
+
+    void format(const char *fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        Console::getInstance().vformat(fmt, args);
+        va_end(args);
+    }
+
+    void flush()
+    {
+        Console::getInstance().flush();
+    }
+
+} // namespace
---
+++ src/console/Console.h
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2026 Dmitry Korobkov <dmitry.korobkov.nn@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+#pragma once
+
+#include <Arduino.h>
+
+// Log levels
+#define LOG_LEVEL_DEBUG 4
+#define LOG_LEVEL_WARNING 3
+#define LOG_LEVEL_INFO 2
+#define LOG_LEVEL_ERROR 1
+#define LOG_LEVEL_NONE 0
+
+// Base log macro
+#define _LOG(...) console::Console::getInstance().log(__VA_ARGS__)
+
+// Conditional logging macros
+#if LOG_LEVEL >= LOG_LEVEL_ERROR
+#define LOG_E(...) _LOG(__VA_ARGS__)
+#else
+#define LOG_E(...)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_INFO
+#define LOG_I(...) _LOG(__VA_ARGS__)
+#else
+#define LOG_I(...)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_WARNING
+#define LOG_W(...) _LOG(__VA_ARGS__)
+#else
+#define LOG_W(...)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_DEBUG
+#define LOG_D(...) _LOG(__VA_ARGS__)
+#else
+#define LOG_D(...)
+#endif
+
+// For backward compatibility
+#if LOG_LEVEL >= LOG_LEVEL_INFO
+#define LOG(...) _LOG(__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+namespace console
+{
+    /**
+     * @brief Singleton logger class for console output
+     *
+     * Provides formatted logging with different log levels and
+     * incremental message building capabilities.
+     */
+    class Console
+    {
+    private:
+        Console();
+        ~Console();
+
+        Console(Console const &) = delete;
+        Console &operator=(Console const &) = delete;
+
+    public:
+        /**
+         * @brief Get the singleton instance
+         * @return Reference to the Console instance
+         */
+        static Console &getInstance()
+        {
+            static Console console;
+            return console;
+        }
+
+        /**
+         * @brief Set the serial port for output
+         * @param serial Reference to HardwareSerial object
+         * @return Reference to this Console instance (for chaining)
+         */
+        Console &setSerial(HardwareSerial &serial);
+
+        /**
+         * @brief Set the baud rate for serial communication
+         * @param baudrate Baud rate (e.g., 115200, 9600)
+         * @return Reference to this Console instance (for chaining)
+         */
+        Console &setBaudRate(unsigned long baudrate);
+
+        /**
+         * @brief Append formatted text to the internal buffer
+         * @param fmt printf-style format string
+         * @param ... Variable arguments
+         */
+        void format(const char *fmt, ...);
+
+        /**
+         * @brief Flush the internal buffer to serial output
+         *
+         * Prints the accumulated buffer content with newline
+         * and clears the buffer.
+         */
+        void flush();
+
+        /**
+         * @brief Print formatted message directly to serial
+         * @param fmt printf-style format string
+         * @param ... Variable arguments
+         *
+         * This is equivalent to format() + flush() in one call.
+         */
+        void log(const char *fmt, ...);
+
+        /**
+         * @brief Append formatted text to the internal buffer
+         * @param fmt printf-style format string
+         * @param args va_list of arguments
+         */
+        void vformat(const char *fmt, va_list args);
+
+        /**
+         * @brief Print message to serial directly to serial
+         * @param fmt printf-style format string
+         * @param args va_list of arguments
+         */
+        void vlog(const char *fmt, va_list args);
+
+    private:
+        HardwareSerial *serial; // Pointer to serial port
+        unsigned long baudrate; // Current baud rate
+        char mBuffer[96];       // Internal buffer for message building
+    };
+
+    // Console wrapper functions
+
+    void log(const char *fmt, ...);
+    void format(const char *fmt, ...);
+    void flush();
+
+} // namespace
